bd3e2bfd2ba586993950068af5515a944fda9a82
Harshu0132 <harshalmaheshram10@gmail.com>
2023-12-11 21:02:14 +0530

    new code added

--- routes/search/searchRoutes.js
+++ routes/search/searchRoutes.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const searchController = require('../../controller/searchContoller')
 router.get('/searchByProduct/:product', searchController.searchByProduct)
 router.get('/searchByCategory/:category', searchController.searchByCategory)
+router.get('/getProductDetails/:id', searchController.getProductDetailsById)
 
 module.exports = router
