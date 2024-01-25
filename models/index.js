e77451f6c2638d361efca839ccba9759dbefa1b5
krunalcyanide <krunalgaykwad2002@gmail.com>
2024-01-25 13:24:46 +0530

    module js

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const DB = 'mongodb+srv://Croful:croful0132@cluster0.uki58l9.mongodb.net/'
+
+mongoose.connect(DB, {
+    family: 4
+}).then(() => {
+    console.log('connected');
+}).catch((error) => {
+    console.log('Connection failed', error);
+})
+
+const User = require('./User')
