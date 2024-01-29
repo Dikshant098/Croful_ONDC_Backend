c33acbbfc3ac3b560fcf10bffb81401b9a97a137
Dikshant098 <103027816+Dikshant098@users.noreply.github.com>
2024-01-29 16:51:47 +0530

    code update

--- controller/locationController.js
+++ controller/locationController.js
@@ -6,14 +6,14 @@ const getLocation = async (req, res) => {
     try {
         // Replace 'YOUR_API_KEY' with your actual API key from OpenCage
         // const apiKey = 'pk.e4e118b80d0c2e10e968cd0c79316d38';
-        const apiKey = '11ee01b6e6e34c719bb71e3c53a8bc86';
+        const apiKey = 'pk.01c0b45713c6b90a6fc19cd10c0c4e2a';
      
         // console.log(response);
         const response = await fetch(
             // `https://us1.locationiq.com/v1/reverse.php?key=${apiKey}&lat=${latitude}&lon=${longitude}&format=json`
-            // `https://us1.locationiq.com/v1/reverse.php?key=${apiKey}&lat=${latitude}&lon=${longitude}&format=json`
+            `https://us1.locationiq.com/v1/reverse.php?key=${apiKey}&lat=${latitude}&lon=${longitude}&format=json`
             // `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`
-            `https://api.opencagedata.com/geocode/v1/json?q=${latitude}%2C${longitude}&key=11ee01b6e6e34c719bb71e3c53a8bc86`
+            // `https://api.opencagedata.com/geocode/v1/json?q=${latitude}%2C${longitude}&key=11ee01b6e6e34c719bb71e3c53a8bc86`
         );
         // console.log(response);
         if (!response.ok) {
@@ -25,7 +25,7 @@ const getLocation = async (req, res) => {
 
         // console.log(data.results.components)
 
-        res.send(data.results)
+        res.send(data)
 
     } catch (error) {
         console.error('Error fetching location data:', error.message);
--- models/index.js
+++ models/index.js
@@ -10,3 +10,5 @@
 // })
 
 // const User = require('./User')
+
+
